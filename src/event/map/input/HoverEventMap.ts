5393dbbcfea77cfd88a9382ac55ac020eded5857
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-09 19:31:25 +0900

    remove hover

--- src/event/map/input/HoverEventMap.ts
+++
@@ -1,8 +0,0 @@
-import EventMessage from '../EventMessage';
-
-export default interface KeyboardEventMap {
-  keydown: HoverEvent;
-  keyup: HoverEvent;
-}
-
-interface HoverEvent extends EventMessage, MouseEvent {}
