8e21f142a3f2885619aa47a5c45eba2e828784b0
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-09 19:27:59 +0900

    character

--- src/logic/module/DevMod.ts
+++ src/logic/module/DevMod.ts
@@ -1,8 +1,10 @@
 import DummyMap from '../loader/DummyMap';
 import Module from '$/package/Module';
+import Character from '../loader/Character';
 
 export default class DevMod extends Module {
   onSet() {
     this.addChild(DummyMap);
+    this.addChild(Character);
   }
 }
