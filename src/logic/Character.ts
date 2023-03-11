76f37052a894b701d48391b789cf97d2d8478b5c
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-03-12 00:01:39 +0900

    Update Character.ts

---
+++ src/logic/Character.ts
@@ -0,0 +1,36 @@
+import Loader from './Loader';
+import character from '$static/model/character.glb';
+import { TransformNode, Vector3 } from 'babylonjs';
+import AnimateControl from '$/util/AnimateControl';
+
+export default class Character extends Loader {
+  protected url: [string, string] = ['./', character];
+
+  protected onSet(): void {
+    this.addEventHandler('onLoad', ({ assets }) => {
+       assets.addAllToScene();
+      const scene = this.getScene();
+      const character = assets.getNodes()[0] as TransformNode;
+      character.position = scene
+      .getTransformNodeByName('SpawnPoint')!
+
+      .position.clone();
+      const animation = new AnimateControl<Vector3>(character.position);
+      this.addEventHandler('keydown',(e)=>{
+        if(e.key=='l'){
+
+          animation.addAnimate(new Vector3(0,0,-3),1000);
+        }
+      })
+      // this.addEventHandler('update',(e)=>{character.position.z -= 0.01*e.deltaTime})
+      // const animation = new AnimateControl<Vector3>(character.position);
+      // this.addEventHandler('keydown', (e) => {
+      //   if (e.key == ' ') animation.addAnimate(new Vector3(0, 0, -2), 200);
+      // });
+      this.addEventHandler('update', (e) => {
+        animation.update(e.deltaTime);
+      });
+    });
+    this.load();
+  }
+}
