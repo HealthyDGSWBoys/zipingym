049766fe156d1b38aaa59492eecc425a0a5c0099
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-24 08:28:54 +0900

    [STYLE] remove useless comment

--- src/class/keyframe/KeyframeAnimation.ts
+++ src/class/keyframe/KeyframeAnimation.ts
@@ -5,24 +5,11 @@ import CustomAnimation from './CustomAnimation';
 export default class KeyframeAnimation implements CustomAnimation {
   private target: TransformNode;
   private type: 'position' | 'rotation';
-  // private animationQueue = new Array<{
-  //   onmillsec: Vector3;
-  //   duration: number;
-  // }>();
   constructor(target: TransformNode, type: 'position' | 'rotation') {
     this.target = target;
     this.type = type;
   }
   public animate(type: 'add' | 'set', vector: Vector3, duration: number) {
-    // const delta =
-    //   type == 'add'
-    //     ? vector.clone()
-    //     : vector.clone().subtract(this.target[this.type]);
-    // this.animationQueue.push({
-    //   onmillsec: delta.divide(new Vector3().setAll(duration)),
-    //   duration,
-    // });
-
     BABYLON.Animation.CreateMergeAndStartAnimation(
       String(Math.random()),
       this.target,
@@ -34,16 +21,4 @@ export default class KeyframeAnimation implements CustomAnimation {
       BABYLON.Animation.ANIMATIONLOOPMODE_RELATIVE
     );
   }
-  // public update(deltaTime: number) {
-  //   this.animationQueue.forEach(({ onmillsec, duration }, idx) => {
-  //     const delta = duration - deltaTime < 0 ? duration : deltaTime;
-  //     this.target[this.type] = this.target[this.type].add(
-  //       onmillsec.clone().multiply(new Vector3().setAll(delta))
-  //     );
-  //     this.animationQueue[idx].duration -= deltaTime;
-  //     if (duration <= 0) {
-  //       this.animationQueue.splice(idx, 1);
-  //     }
-  //   });
-  // }
 }
