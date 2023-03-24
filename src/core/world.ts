44969e5aeddbaa144b4aea9e94b6b75e864b0122
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-25 01:48:50 +0900

    [ADD] load thema map

--- src/core/world.ts
+++ src/core/world.ts
@@ -1,19 +1,25 @@
 import Core from './core';
 import dummyMap from '$static/model/dummy.glb';
-import { AssetContainer } from 'babylonjs';
+import start from '$static/model/start.glb';
+import testThema from '$static/model/testmap.glb';
+import { AssetContainer, Mesh, TransformNode } from 'babylonjs';
 import { LoadAll } from '$/function/Load';
+import WorldManager from '$/class/world/worldManager';
 
 export default class World extends Core {
   private static WorldModelFile: Map<string, string> = new Map([
     ['dummy', dummyMap],
+    ['start', start],
+    ['testThema', testThema],
   ]);
   private worldModel: Map<string, AssetContainer>;
+  private worldManagers: Map<string, WorldManager> = new Map();
   public set: () => Promise<void> = () => {
     return new Promise((resolve, reject) => {
       LoadAll(World.WorldModelFile, this.scene)
         .then((world) => {
           this.worldModel = world;
-          const dummyMap = this.worldModel.get('dummy');
+          const dummyMap = this.worldModel.get('start');
           if (dummyMap != undefined) {
             dummyMap.addAllToScene();
             resolve();
@@ -24,6 +30,18 @@ export default class World extends Core {
         .catch(reject);
     });
   };
-  public setsync = () => {};
+  public setsync = () => {
+    const testThema = this.worldModel.get('testThema');
+    if (testThema != undefined) {
+      this.worldManagers.set('test', new WorldManager(testThema));
+    }
+    const testManager = this.worldManagers.get('test')!.list;
+
+    const spawnpoint = this.scene.getNodeByName('SpawnPoint') as Mesh;
+
+    const newRoad = testManager[0].clone();
+    newRoad.parent = spawnpoint;
+    newRoad.position.set(0, 0, -10);
+  };
   public loop = (deltaTime: number) => {};
 }
