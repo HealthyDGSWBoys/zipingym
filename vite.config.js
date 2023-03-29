73d6af5707e9cdc4a5bd89d6d4a5d6ccb3af6c6d
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-03-29 11:40:05 +0900

    [build] visualizer

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,22 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import gltf from 'vite-plugin-gltf';
 import wasm from 'vite-plugin-wasm';
 import top_await from 'vite-plugin-top-level-await';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig(({ command, mode }) => {
   return {
     assetsInclude: ['**/*.tflite'],
-    plugins: [gltf(), wasm(), top_await(), tsconfigPaths()],
+    plugins: [
+      gltf(),
+      wasm(),
+      top_await(),
+      visualizer({
+        filename: './dist/report.html',
+        open: true,
+        brotliSize: true,
+      }),
+      tsconfigPaths(),
+    ],
     define: {
       __ISPRODUCTION__: mode !== 'development',
     },
