447ac9e036e2c5bb9fb9905798f6ca012183fb2f
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-04-10 09:59:14 +0900

    [STYLE] remove useless import

--- src/package/command/Command.ts
+++ src/package/command/Command.ts
@@ -1,10 +1,7 @@
 import { Update } from '$/global/Updateable';
 import Config from '$/static/config/Config';
 import User from '../character/User';
-import InputController from '../character/InputController';
-import InputFactory, { InputType } from '../input/InputFactory';
 import World from '../world/World';
-import WorldRenderController from '../world/WorldRenderController';
 
 export default class Command extends Update {
   constructor() {
