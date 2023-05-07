95a2b43c0e72c1a8a5a9fecaba8e00a66b534460
박병관 <parkbyoungkwan@bagbyeong-gwans-MacBook-Pro.local>
2023-05-07 16:10:56 +0900

    [ADD] click on modal

--- src/core/UICore/ModalElement/ModalElement.ts
+++ src/core/UICore/ModalElement/ModalElement.ts
@@ -3,17 +3,40 @@ import "./ModalElement.style.scss"
 
 /** @todo 싱글톤으로 하고싶었는데 일단 못 함 */
 export default class ModalElement extends ClickAbleCustomElement{
-    private static readonly isOpen:string = "0"
-    private static readonly element:string
-    private static readonly outsideClickEffect:string
-    // public static instance:ModalElement = new ModalElement();
-    // private constructor(){super()}
-    connectedCallback(){
-        
 
+    private serializer = new XMLSerializer();
+
+    connectedCallback(){
+        this.changeModalAttribute({
+            isOpen:false,
+            element:new HTMLElement(),
+            outsideClickEffect:() => {},
+            isDark:true
+        })
     }
 
-    changeModalAttribute(isOpen:boolean,element:HTMLElement,outsideClickEffect:Function){
+
+    changeModalAttribute({isOpen,element,outsideClickEffect,isDark}:{isOpen:boolean,element:HTMLElement,outsideClickEffect?:EventListenerOrEventListenerObject, isDark?:boolean}){
+        if (isOpen){this.style.zIndex = "1"}
+        else {this.style.zIndex = "0"}
+
+        if (element){
+            this.clearDom()
+            console.log(this.serializer.serializeToString(element))
+            this.addInnerHtmlToThis(this.serializer.serializeToString(element))
+        } else {
+            this.clearDom()
+        }
+
+        if(outsideClickEffect){
+            this.useClickEffects([{
+               selector:null,
+               FN:outsideClickEffect 
+            }])
+        }
+
+        if (isDark){this.style.backgroundColor="rgba($color: #000000, $alpha: 0.7)"}
+        else {this.style.backgroundColor=""}
 
     }
 
