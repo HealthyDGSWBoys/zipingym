a8c7395bb092049789f78ef43b8da5c1abe736b7
kim geon ho <80884472+qfourca@users.noreply.github.com>
2023-05-08 10:55:17 +0900

    [FIX] array to code

--- src/module/input/ExerciseInput/trigger/DumbleTrigger.ts
+++ src/module/input/ExerciseInput/trigger/DumbleTrigger.ts
@@ -79,12 +79,12 @@ export default class DumbleTrigger extends Trigger {
 
   public arrayToCode(arr: number[]): number {
     const line = 0.5;
-    if (arr[0] > line && arr[1] > line) {
-      return 3;
-    } else if (arr[0] <= line && arr[1] <= line) {
-      return 0;
-    } else {
+    if (Math.abs(arr[0] - arr[1]) > 1.5) {
       return arr[0] > arr[1] ? 1 : 2;
+    } else if (arr[0] > line && arr[1] > line) {
+      return 3;
+    } else {
+      return 0;
     }
   }
 
